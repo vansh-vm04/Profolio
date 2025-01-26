25779d42e225a750d76c6d1af6e02a568931e0d2
Vansh <vanshmorwani1@gmail.com>
2025-01-26 21:40:11 +0530

    button fix

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             <button
               onClick={() => navigate("/dashboard")}
               type="button"
-              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-4 py-1"
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg max-md:px-1 px-4 py-1"
             >
               My Dashboard
             </button>
